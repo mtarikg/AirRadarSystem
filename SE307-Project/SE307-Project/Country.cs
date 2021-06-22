2d7de3774cbb3d42fa805b8e96c0459c430cf48b
obadaqasem <49880061+obadaqasem@users.noreply.github.com>
2021-06-23 00:21:03 +0300

    Missiles Specilization Completion and Radar is completed Manager Movement Completed Missiles Inheritance Completed Log Files Methods Completed

--- SE307-Project/SE307-Project/Country.cs
+++ SE307-Project/SE307-Project/Country.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace SE307_Project
 {
@@ -11,6 +12,9 @@ namespace SE307_Project
         private AirStation airStation; // a country must have an air station.
         private List<AirCraft> aircrafts; // a country can have multiple aircrafts.
         private List<Missile> missiles; // a country can have multiple missiles.
+        private List<Country> alliesCountries = new List<Country>();
+        private List<Country> enemyCountries = new List<Country>();
+
 
         public Country(int id, string countryName, double population, double area, AirStation airStation, List<AirCraft> aircrafts, List<Missile> missiles)
         {
@@ -23,6 +27,10 @@ namespace SE307_Project
             this.missiles = missiles;
         }
 
+        public Country()
+        {
+        }
+
         public int Id { get => id; set => id = value; }
         public string CountryName { get => countryName; set => countryName = value; }
         public double Population { get => population; set => population = value; }
@@ -30,5 +38,17 @@ namespace SE307_Project
         public AirStation AirStation { get => airStation; set => airStation = value; }
         public List<AirCraft> Aircrafts { get => aircrafts; set => aircrafts = value; }
         public List<Missile> Missiles { get => missiles; set => missiles = value; }
+
+        public List<Country> AlliesCountries
+        {
+            get => alliesCountries;
+            set => alliesCountries = value;
+        }
+
+        public List<Country> EnemyCountries
+        {
+            get => enemyCountries;
+            set => enemyCountries = value;
+        }
     }
 }
---
+++ SE307-Project/SE307-Project/LightMissiles.cs
@@ -0,0 +1,37 @@
+﻿namespace SE307_Project
+{
+    public class LightMissiles:Missile
+    {
+        private string type;
+        private int powerRank; // from 1-10 10 is the most powerful for the light Missiles
+        
+
+        public int PowerRank
+        {
+            get => powerRank;
+            set => powerRank = value;
+        }
+
+        public string Type
+        {
+            get => type;
+            set => type = value;
+        }
+
+        public LightMissiles(int id, double speed, string type, int powerRank) : base(id, speed)
+        {
+            this.type = type;
+            this.powerRank = powerRank;
+        }
+         public override bool checkTheHittingPercent()
+           {
+                 throw new System.NotImplementedException();  
+             }
+
+       
+
+        public LightMissiles()
+        {
+        }
+    }
+}
